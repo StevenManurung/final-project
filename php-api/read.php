bb41f7fca5221077e5f0beb7018cfde7fd5acf0b
trifine <trifine.laurensi08@gmail.com>
2023-12-20 22:15:54 +0700

    Menambahkan api untuk membaca data

---
+++ php-api/read.php
@@ -0,0 +1,26 @@
+<?php
+  require_once('inc/open_connection.php');
+  
+  $result = [];
+
+  $query = "SELECT id AS id,
+            nama AS nama, 
+            nohp AS nohp, 
+            provinsi AS provinsi, 
+            kota AS kota, 
+            kecamatan AS kecamatan,
+            kodepos AS kodepos,
+            namajalan AS namajalan,
+            detailalamat AS detailalamat
+            FROM alamat_user";
+
+  $query_run = mysqli_query($koneksi, $query);
+
+  while ($row = mysqli_fetch_assoc($query_run)) {
+    $result[] = $row;
+  }
+
+  echo json_encode($result);
+
+  require_once('inc/close_connection.php');
+?>
