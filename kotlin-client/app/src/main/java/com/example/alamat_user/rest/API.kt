38d26c7eb2004c035cb8d583aa987113ee3ad3f7
StevenManurung <Steven_mrng43@gmail.com>
2023-12-20 21:25:45 +0700

    first commit steven-consumeApi

--- kotlin-client/app/src/main/java/com/example/alamat_user/rest/API.kt
+++ kotlin-client/app/src/main/java/com/example/alamat_user/rest/API.kt
@@ -5,44 +5,7 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface API {
-    @GET("read.php")
-    fun getAlamatt():Call<ArrayList<AlamatItem>>
+    @TODO
 
-    @GET("detail.php")
-    fun getAlamatDetail(
-        @Query("id") id: String?
-    ):Call<AlamatDetail>
-
-    @FormUrlEncoded
-    @POST("add.php")
-    fun addAlamatDetail(
-        @Field("nama") nama: String?,
-        @Field("nohp") nohp: String,
-        @Field("provinsi") provinsi: String?,
-        @Field("kota") kota: String?,
-        @Field("kecamatan") kecamatan: String?,
-        @Field("kodepos") kodepos: String,
-        @Field("namajalan") namajalan: String?,
-        @Field("detailalamat") detailalamat: String?
-    ): Call<Response>
-
-    @FormUrlEncoded
-    @POST("update.php")
-    fun ubahAlamatDetail(
-        @Field("id") id: String?,
-        @Field("nama") nama: String?,
-        @Field("nohp") nohp: String?,
-        @Field("provinsi") provinsi: String?,
-        @Field("kota") kota: String?,
-        @Field("kecamatan") kecamatan: String?,
-        @Field("kodepos") kodepos: String?,
-        @Field("namajalan") namajalan: String?,
-        @Field("detailalamat") detailalamat: String?
-    ): Call<Response>
-
-    @FormUrlEncoded
-    @POST("delete.php")
-    fun deleteAlamatDetail(
-        @Field("id") id: String?
-    ): Call<Response>
+    // first commit steven-consumeApi
 }
